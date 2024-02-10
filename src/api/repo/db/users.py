9c60d857b951083f86719d38395edd4bdc1515cd
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-02-10 16:02:16 -0600

    Database skeleton methods for users

---
+++ src/api/repo/db/users.py
@@ -0,0 +1,23 @@
+from typing import Self
+
+class Users:
+    def __init__(self, id: str, name: str):
+        self.id = id
+        self.name = name
+
+    @classmethod
+    def get_all(cls) -> list[Self]:
+        return cls("1", "John")
+
+    @classmethod
+    def get_by_ids(cls, user_ids: str) -> list[Self]:
+        return [cls(user_id, "John") for user_id in user_ids]
+
+    def create(self):
+        pass
+
+    def update(self, **args):
+        pass
+
+    def delete(self):
+        pass
