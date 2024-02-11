966836a7af6c7c80a5a70d6f968468cf16e647d9
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-02-10 23:27:56 -0600

    Postgres connection

---
+++ src/api/repo/db/psql_conn.py
@@ -0,0 +1,19 @@
+import os
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DB_USER = os.environ.get("DB_USER", "user")
+DB_PASS = os.environ.get("DB_PASS", "password")
+DB_HOST = os.environ.get("DB_HOST", "database")
+DB_PORT = os.environ.get("DB_PORT", 5432)
+DB_NAME = os.environ.get("DB_NAME", "db")
+
+engine = create_async_engine(
+    f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
+    future=True,
+    isolation_level="AUTOCOMMIT",
+)
+
+session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+Base = declarative_base()
