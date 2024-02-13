522cd9cd5521b8bc5865235cad651c272dc1fed5
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-02-12 23:10:18 -0600

    Back to sync database (hybrid WIP)

--- migrations/env.py
+++ migrations/env.py
@@ -1,10 +1,13 @@
 import os
+import asyncio
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+from main import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -32,7 +35,7 @@ DB_HOST = os.environ.get("DB_HOST", "database")
 DB_PORT = os.environ.get("DB_PORT", 5432)
 DB_NAME = os.environ.get("DB_NAME", "db")
 
-config.set_main_option("sqlalchemy.url", f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+config.set_main_option("sqlalchemy.url", f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -76,7 +79,6 @@ def run_migrations_online() -> None:
 
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
--- src/api/repo/conn/sql.py
+++ src/api/repo/conn/sql.py
@@ -1,7 +1,8 @@
 import os
 
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
 
 DB_USER = os.environ.get("DB_USER", "user")
 DB_PASS = os.environ.get("DB_PASS", "password")
@@ -9,11 +10,12 @@ DB_HOST = os.environ.get("DB_HOST", "database")
 DB_PORT = os.environ.get("DB_PORT", 5432)
 DB_NAME = os.environ.get("DB_NAME", "db")
 
-engine = create_async_engine(
-    f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
+engine = create_engine(
+    f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
     future=True,
     isolation_level="AUTOCOMMIT",
 )
 
-session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+session = sessionmaker(engine, expire_on_commit=False)
 Base = declarative_base()
+# Base.metadata.create_all(engine)
