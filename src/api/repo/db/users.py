f9da806a5546f5a6d2007754d7969d5f59fce5a8
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-02-12 23:09:31 -0600

    Fixes in orm object

--- src/api/repo/db/users.py
+++ src/api/repo/db/users.py
@@ -4,18 +4,18 @@ from sqlalchemy import Column, String
 
 
 class Users(Base):
-    __tablename__ = "bas_ficax_services_view"
+    __tablename__ = "user"
 
-    user_id = Column("USER_ID", String(20), primary_key=True)
+    id = Column("USER_ID", String(20), primary_key=True)
     name = Column("NAME", String(50))
 
     @classmethod
     def get_all(cls) -> list[Self]:
-        return cls("1", "John")
+        return [cls(id="1", name="John")]
 
     @classmethod
     def get_by_ids(cls, user_ids: str) -> list[Self]:
-        return [cls(user_id, "John") for user_id in user_ids]
+        return [cls(id, "John") for user_id in user_ids]
 
     def create(self):
         pass
