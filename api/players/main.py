0a0273f5b4fa8b8af573a04b8fba0f8a9b77d474
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-02-23 23:28:42 -0600

    Issue #9: Add CORS to players

--- api/players/main.py
+++ api/players/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.routers import health_checker_router, user_router
 from src.repo.conn import Base
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["Content-Type"],
+    allow_credentials="true",
+)
 
 app.include_router(user_router)
 app.include_router(health_checker_router)
