08dd83927e957e1c4df6204b5702c66e64232811
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-02-24 11:44:27 -0600

    Issue #15: Add logger to api

--- api/players/src/routers/health.py
+++ api/players/src/routers/health.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
+from src.utils.logger import logger
 
 router = APIRouter(prefix="/health", tags=["Server"])
 
 
 @router.get("/")
 def health_check():
+    logger.info("Checking health")
     return {"status": "up"}
