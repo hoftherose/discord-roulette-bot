26f74e700ba945a3f8d338912b767f00af826281
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-02-23 23:29:31 -0600

    Issue #9: CORS to admin service

--- api/admin/main.py
+++ api/admin/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.routers import health_checker_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["Content-Type"],
+    allow_credentials="true",
+)
 
 app.include_router(health_checker_router)
