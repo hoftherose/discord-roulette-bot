81cd8f4b1c306ca780fb4bbf3775a38a5e207eaf
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-05-21 13:17:13 -0600

    Issue #12: User list response object

---
+++ api/players/src/routers/responses/users_response.py
@@ -0,0 +1,25 @@
+"""Response models for client endpoint"""
+from datetime import date
+from typing import List
+from pydantic import BaseModel
+
+from .systemcodes import SystemCodes
+from src.routers.responses.base import BaseResponse
+
+
+class UserSchema(BaseModel):
+    """Schema for returning users"""
+
+    name: str
+
+
+class UserListResponse(BaseResponse):
+    """Response to return next payment dates in specified range."""
+
+    message: str = "Returned due date in specified range"
+    code: int = SystemCodes.SUCCESSFUL_QUERY
+
+    class Schema(BaseModel):
+        """Data Schema"""
+
+        RootModel: List[UserSchema]
