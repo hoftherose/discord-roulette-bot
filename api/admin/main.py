b33d9ef630845be5be84906ca5fc9996568b28da
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-05-24 12:56:31 -0600

    Issue #16: Add changes to admin

--- api/admin/main.py
+++ api/admin/main.py
@@ -1,9 +1,31 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
 from src.routers import health_checker_router
 
+provider = TracerProvider(
+    resource=Resource.create({SERVICE_NAME: os.environ.get("OTEL_SERVICE_NAME")})
+)
+processor = BatchSpanProcessor(
+    JaegerExporter(
+        agent_host_name=os.environ.get("JAEGER_HOST", "jaeger"),
+        agent_port=os.environ.get("JAEGER_PORT", 6831),
+    )
+)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
 app = FastAPI()
+FastAPIInstrumentor.instrument_app(app, excluded_urls="/health,/docs,/openapi.json")
 
 app.add_middleware(
     CORSMiddleware,
